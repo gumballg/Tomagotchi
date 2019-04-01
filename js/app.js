2b9a68fa11e5a658992aee4db751b87b2dac85f0
Matthew Gonczar <mattisagumball@gmail.com>
2019-04-01 15:30:22 -0500

    created tomagotchi class

---
+++ js/app.js
@@ -0,0 +1,22 @@
+class Tomagotchi {
+	constructor(name){
+		this.hunger = 1;
+		this.sleep = 1;
+		this.boredom = 1;
+		this.age = 1;
+		this.name = 'name';
+		this.alive = true;
+	}
+	feed(){
+
+	}
+	lights(){
+
+	}
+	play(){
+
+	}
+	die(){
+
+	}
+}
